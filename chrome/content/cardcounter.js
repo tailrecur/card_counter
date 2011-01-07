65e8d4f97ee1cd59c7475198d1586dbed641c0e9
dlewis <deepak.lewis@gmail.com>
2011-01-07 16:29:06 +0530

    Right click marks card as red

--- chrome/content/cardcounter.js
+++ chrome/content/cardcounter.js
@@ -1,16 +1,23 @@
+var joker = nil;
+
 function toggle(event) {
-  classToApply = event.which == 3 ? "picked" : "hidden";
-  $(event.target).toggleClass(classToApply);
+  $(event.target).toggleClass(leftClick(event) ? "hidden" : "picked");
+  // if(leftClick(event) && !joker) {
+  //   joker = $(event.target).label;
+  // }
+  // alert(joker);
 }
 
 function launchCounter() {
   $('#counterWindow').toggle(); 
-  $("row>toolbarbutton.plain").each(function() { $(this).click(function(event) { toggle(event); } ); });
+  $("row>toolbarbutton.plain").each(function() { $(this).click(function(event) { toggle(event); }); });
 }
 
 function reset() {
   $("row>toolbarbutton.plain").each(function() { $(this).removeClass("hidden").removeClass("picked"); });
+  joker = nil;
 }
 
-
-
+function leftClick(event) {
+  return event.which == 1
+}
